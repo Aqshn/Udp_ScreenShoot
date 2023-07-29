84ac9148f5dc5416a7ef91679b05bae938685568
user <user@DESKTOP-BE8CPQG>
2023-07-29 01:56:43 -0700

    Add project files.

---
+++ Udp_Client/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Udp_Client
+{
+    public partial class MainWindow : Window
+    {
+        byte[]? imagebuffer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 27001;
+            var remoteEP = new IPEndPoint(ip, port);
+
+            var size = 0;
+            var buffer = Array.Empty<byte>();
+
+            buffer = new byte[1] { 4 };
+            await client.SendToAsync(new ArraySegment<byte>(buffer), 
+                SocketFlags.None, 
+                remoteEP);
+            buffer = new byte[ushort.MaxValue - 29];
+            var result = await client.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+                SocketFlags.None,
+                remoteEP);
+            size = result.ReceivedBytes;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                str.Append(((char)buffer[i]));
+            }
+
+
+            var imagesize = int.Parse(str.ToString());
+            imagebuffer = new byte[imagesize];
+            size = 0;
+            var receivesize = 0;
+            var index = 0;
+
+            while (imagesize > size)
+            {
+                buffer = new byte[1];
+                await client.SendToAsync(new ArraySegment<byte>(buffer),
+                    SocketFlags.None,
+                    remoteEP);
+                buffer = new byte[ushort.MaxValue - 29];
+                result = await client.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+                    SocketFlags.None,
+                    remoteEP);
+                receivesize = result.ReceivedBytes;
+                buffer.ToList().CopyTo(0, imagebuffer, index, receivesize);
+                size += receivesize;
+                index = size - 1;
+            }
+            BuildImage(imagebuffer);
+
+        }
+
+        public void BuildImage(byte[] imagebuffer)
+        {
+            MemoryStream ms = new MemoryStream(imagebuffer);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = ms;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.EndInit();
+            ScreenImage.Source = image;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScreenImage.Source is not null)
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "PNG Files(*.png) | *.png";
+
+                if (dialog.ShowDialog() == true)
+                {
+                    File.WriteAllBytes(dialog.FileName, imagebuffer);
+                    MessageBox.Show("Image Saved");
+                    ScreenImage.Source = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No image");
+            }
+        }
+    }
+}
---
+++ Udp_Server/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+
+
+byte[] GetScreenshot()
+{
+    [DllImport("user32.dll")]
+    static extern int GetSystemMetrics(int nIndex);
+
+    const int SM_CXSCREEN = 0;
+    const int SM_CYSCREEN = 1;
+
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    Bitmap Image = new Bitmap(screenWidth, screenHeight);
+
+    Size s = new Size(Image.Width, Image.Height);
+
+
+    using (Graphics memoryGraphics = Graphics.FromImage(Image))
+    {
+        memoryGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+        memoryGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        memoryGraphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+        memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+        using (MemoryStream ms = new MemoryStream())
+        {
+            Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            Image.Dispose();
+            return ms.ToArray();
+        }
+    }
+}
+
+
+
+
+
+var listener = new Socket(AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+var Ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var endpoint = new IPEndPoint(Ip, port);
+
+listener.Bind(endpoint);
+
+EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+
+var buffer = new byte[ushort.MaxValue - 29];
+
+
+int count = 0;
+
+
+
+
+
+
+while (true)
+{
+    listener.ReceiveFrom(buffer, ref remoteEP);
+    var imagebuffer = GetScreenshot();
+
+    var fullen = imagebuffer.Length.ToString();
+    buffer = new byte[fullen.Length];
+    foreach (var item in fullen)
+    {
+        buffer[count++] = (byte)item;
+    }
+    listener.SendTo(buffer, remoteEP);
+    count = 0;
+    var receivesize = 0;
+    var index = 0;
+
+
+    while (imagebuffer.Length > count)
+    {
+        buffer = new byte[ushort.MaxValue - 29];
+        var sendsize = buffer.Length;
+
+        if (imagebuffer.Length - index < sendsize)
+        {
+            sendsize = imagebuffer.Length - count;
+            buffer = new byte[sendsize];
+        }
+        listener.Receive(buffer);
+
+        imagebuffer.ToList().CopyTo(index, buffer, 0, sendsize);
+        receivesize = listener.SendTo(buffer, remoteEP);
+        count += receivesize;
+        index = count - 1;
+    }
+    buffer = new byte[ushort.MaxValue - 29];
+    count = 0;
+}
